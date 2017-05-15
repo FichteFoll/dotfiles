3181a18d269a741219db9508d9214828f33ba3a3
FichteFoll <fichtefoll2@googlemail.com>
2017-05-15 16:08:53 +0200

    Add Sublime Text configuration

---
+++ sublime-text/.config/sublime-text-3/Packages/User/bootstrap_logging.py
@@ -0,0 +1,49 @@
+from functools import partial
+from collections import defaultdict
+
+import sublime
+
+function_names = ('input', 'commands', 'result_regex', 'indexing')
+
+# Cleanup
+if hasattr(sublime, 'logging_unbootstrap'):
+    sublime.logging_unbootstrap()
+
+# Assure this exists
+if not hasattr(sublime, 'logging_cache'):
+    setattr(sublime, 'logging_cache', defaultdict(bool))
+
+# Collect original functions
+original_functions = {name: getattr(sublime, "log_" + name)
+                      for name in function_names}
+
+
+# Override functions with our new function
+def toogle_logging(name, value=None):
+    if value is None:
+        value = not sublime.logging_cache[name]
+    sublime.logging_cache[name] = value
+    original_functions[name](value)
+
+for name in function_names:
+    setattr(sublime, "log_" + name, partial(toogle_logging, name))
+
+
+# Register cleanup
+def unbootstrap():
+    for name, func in original_functions.items():
+        setattr(sublime, "log_" + name, func)
+    delattr(sublime, 'logging_unbootstrap')
+
+    print("Note: Original functionality for sublime.log_* methods has been "
+          "restored")
+
+setattr(sublime, 'logging_unbootstrap', unbootstrap)
+
+# Done
+print("Note: sublime.log_* methods have been overridden and now toggle when "
+      "called without a parameter")
+
+
+def plugin_unloaded():
+    unbootstrap()
---
+++ sublime-text/.config/sublime-text-3/Packages/User/python_print.py
@@ -0,0 +1,32 @@
+import sublime
+import sublime_plugin
+
+
+class PythonPrintCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        sel = self.view.sel()
+        regions = [reg if not reg.empty() else self.view.word(reg) for reg in sel]
+        self.view.add_regions("old_sel", regions)
+
+        # Apply in reverse to not mess up our previously saved regions
+        # This is also necessary because we need to change the selection
+        for reg in reversed(regions):
+            selected_text = self.view.substr(reg)
+            if not selected_text:
+                continue
+            text = ('\nprint("%s: {}".format(%s))'
+                    % (selected_text.replace('\\', '\\\\').replace('"', '\\"'),
+                       selected_text)
+                    )
+            # view.insert does not auto-indent, so we run the insert command
+            # self.view.insert(edit, line.end(), text)
+            sel.clear()
+            line = self.view.line(reg)
+            sel.add(sublime.Region(line.end(), line.end()))
+            self.view.run_command('insert', {'characters': text})
+
+        # Restore old selection (additionally, the expanded selection
+        # if a selection was empty before)
+        sel.clear()
+        sel.add_all(self.view.get_regions("old_sel"))
+        self.view.erase_regions("old_sel")

4a3eea25995c470a48e7b96e888ae778180ee415
FichteFoll <fichtefoll2@googlemail.com>
2018-01-24 16:44:57 +0100

    [mpv] Make _get_default-input executable

--- mpv/.config/mpv/_get_default-input.sh
+++ mpv/.config/mpv/_get_default-input.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # curl -L https://github.com/mpv-player/mpv/raw/master/etc/input.conf -o default-input_.conf
 perl -pe 's/^#(?!#|\s)//g' < /usr/share/doc/mpv/input.conf > default-input.conf
 # rm default-input_.conf

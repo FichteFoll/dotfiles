56d65bcc60032e4ed019da3d4a7954fc95a1b0b5
FichteFoll <fichtefoll2@googlemail.com>
2018-08-23 23:32:56 +0200

    [BD] Update BetterDiscord config

--- BetterDiscord/.config/BetterDiscord/_commit.sh
+++ BetterDiscord/.config/BetterDiscord/_commit.sh
@@ -4,5 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 git add plugins/ themes/
-git add _update.sh _commit.sh
 git commit -m "$(date -Iseconds)"
--- BetterDiscord/.config/BetterDiscord/_update.sh
+++ BetterDiscord/.config/BetterDiscord/_update.sh
@@ -10,9 +10,17 @@ wget -N https://github.com/Arashiryuu/crap/raw/master/greenText.plugin.js
 wget -N https://github.com/kosshishub/BetterCustomCSS-BD-plugin/raw/master/BetterCustomCSS.plugin.js
 wget -N https://gist.github.com/samogot/355ba0ff414a6a9551baff500b1af60e/raw/4251658b52b9591ea6bbd38e1a5309ffd101e266/count-unread.plugin.js
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/linkProfilePicture.plugin.js
+wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/enableReactDevtools.plugin.js
+wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/HighlightSelf.plugin.js
+wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/restartNoMore.plugin.js
 wget -N https://github.com/Finicalmist/CopyCode/raw/master/copyCode.plugin.js
 wget -N https://github.com/Jiiks/BetterDiscordApp/raw/master/Plugins/dblClickEdit.plugin.js
 # wget -N https://gist.github.com/megamit/7a686742c2768f34cf4d83e1055e9375/raw/ee3fd996687707cb1481bee67638bd7f6cbbd302/renamer.plugin.js
+wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/AccountDetailsPlus/AccountDetailsPlus.plugin.js
+wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/PermissionsViewer/PermissionsViewer.plugin.js
+wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/RoleMembers/RoleMembers.plugin.js
+wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/DoNotTrack/DoNotTrack.plugin.js
+wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/BetterFormattingRedux/BetterFormattingRedux.plugin.js
 
 wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/1lib_discord_internals.plugin.js
 wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/no_send_scroll.plugin.js
@@ -27,4 +35,5 @@ wget -N https://github.com/Snow-Pyon/BDStuff/raw/master/themes/BMDAddon/BMDAddon
 wget -N "https://github.com/BeardDesign1/Material-design-theme/raw/themes/Beard's-Material-theme-for-Discord.theme.css"
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/themes/showURLs.theme.css
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/themes/compactUserList.theme.css
-wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Themes/SwitchFix/SwitchFix.theme.css
+wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Themes/RadialStatus/RadialStatus.theme.css
+#wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Themes/SwitchFix/SwitchFix.theme.css  # 404

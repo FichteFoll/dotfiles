b470550f66515c7023c4e224df57cdb4dc77b853
FichteFoll <fichtefoll2@googlemail.com>
2018-10-25 19:21:14 +0200

    [BD] Clean up script downloads

--- BetterDiscord/.config/BetterDiscord/_update.sh
+++ BetterDiscord/.config/BetterDiscord/_update.sh
@@ -5,14 +5,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/plugins"
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/BetterRoleColors/BetterRoleColors.plugin.js
 # wget -N https://github.com/cosmicsalad/Discord-Themes-and-Plugins/raw/master/plugins/replyer.plugin.js
-wget -N https://github.com/cosmicsalad/Discord-Themes-and-Plugins/raw/master/plugins/sorter.plugin.js
+# wget -N https://github.com/cosmicsalad/Discord-Themes-and-Plugins/raw/master/plugins/sorter.plugin.js
 wget -N https://github.com/Arashiryuu/crap/raw/master/greenText.plugin.js
 wget -N https://github.com/kosshishub/BetterCustomCSS-BD-plugin/raw/master/BetterCustomCSS.plugin.js
-wget -N https://gist.github.com/samogot/355ba0ff414a6a9551baff500b1af60e/raw/4251658b52b9591ea6bbd38e1a5309ffd101e266/count-unread.plugin.js
+# wget -N https://gist.github.com/samogot/355ba0ff414a6a9551baff500b1af60e/raw/4251658b52b9591ea6bbd38e1a5309ffd101e266/count-unread.plugin.js
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/linkProfilePicture.plugin.js
-wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/enableReactDevtools.plugin.js
+# wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/enableReactDevtools.plugin.js
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/HighlightSelf.plugin.js
-wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/restartNoMore.plugin.js
+# wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/restartNoMore.plugin.js
 wget -N https://github.com/Finicalmist/CopyCode/raw/master/copyCode.plugin.js
 wget -N https://github.com/Jiiks/BetterDiscordApp/raw/master/Plugins/dblClickEdit.plugin.js
 # wget -N https://gist.github.com/megamit/7a686742c2768f34cf4d83e1055e9375/raw/ee3fd996687707cb1481bee67638bd7f6cbbd302/renamer.plugin.js
@@ -22,13 +22,11 @@ wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/RoleMe
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/DoNotTrack/DoNotTrack.plugin.js
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/BetterFormattingRedux/BetterFormattingRedux.plugin.js
 
+wget -N https://github.com/rauenzi/BDPluginLibrary/raw/release/0PluginLibrary.plugin.js
+
 wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/1lib_discord_internals.plugin.js
 wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/no_send_scroll.plugin.js
 wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/quoter.plugin.js
-
-# v2 stuff
-# wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v2/1Lib%20Discord%20Internals/plugin.js -O 1temp_lib_plugin.plugin.js
-# wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v2/No%20send%20scroll/plugin.js -O no_send_scroll.plugin.js
 
 cd "$DIR/themes"
 wget -N https://github.com/Snow-Pyon/BDStuff/raw/master/themes/BMDAddon/BMDAddon.theme.css
@@ -36,4 +34,3 @@ wget -N "https://github.com/BeardDesign1/Material-design-theme/raw/themes/Beard'
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/themes/showURLs.theme.css
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/themes/compactUserList.theme.css
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Themes/RadialStatus/RadialStatus.theme.css
-#wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Themes/SwitchFix/SwitchFix.theme.css  # 404

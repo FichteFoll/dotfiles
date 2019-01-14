16e06717ccbb064498737c856a37d44b1552e94c
FichteFoll <fichtefoll2@googlemail.com>
2019-01-14 16:34:54 +0100

    [i3+polybar] Use polybar launch script

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
+
+# Launch bars
+polybar primary &
+polybar secondary &
+
+echo "Bars launched..."

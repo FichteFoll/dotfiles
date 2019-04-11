47a2a633e3a2f22898e2d33842fef189dbde9e1c
FichteFoll <fichtefoll2@googlemail.com>
2019-04-12 01:01:56 +0200

    [BD] Sort download urls and add ServerHider

--- BetterDiscord/.config/BetterDiscord/_update.sh
+++ BetterDiscord/.config/BetterDiscord/_update.sh
@@ -3,34 +3,36 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR/plugins"
-wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/BetterRoleColors/BetterRoleColors.plugin.js
+wget -N https://github.com/Arashiryuu/crap/raw/master/greenText.plugin.js
 # wget -N https://github.com/cosmicsalad/Discord-Themes-and-Plugins/raw/master/plugins/replyer.plugin.js
 # wget -N https://github.com/cosmicsalad/Discord-Themes-and-Plugins/raw/master/plugins/sorter.plugin.js
-wget -N https://github.com/Arashiryuu/crap/raw/master/greenText.plugin.js
-wget -N https://github.com/kosshishub/BetterCustomCSS-BD-plugin/raw/master/BetterCustomCSS.plugin.js
-# wget -N https://gist.github.com/samogot/355ba0ff414a6a9551baff500b1af60e/raw/4251658b52b9591ea6bbd38e1a5309ffd101e266/count-unread.plugin.js
-wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/linkProfilePicture.plugin.js
+wget -N https://github.com/Finicalmist/CopyCode/raw/master/copyCode.plugin.js
 # wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/enableReactDevtools.plugin.js
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/HighlightSelf.plugin.js
+wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/linkProfilePicture.plugin.js
 # wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/restartNoMore.plugin.js
-wget -N https://github.com/Finicalmist/CopyCode/raw/master/copyCode.plugin.js
 wget -N https://github.com/Jiiks/BetterDiscordApp/raw/master/Plugins/dblClickEdit.plugin.js
+wget -N https://github.com/kosshishub/BetterCustomCSS-BD-plugin/raw/master/BetterCustomCSS.plugin.js
+wget -N https://github.com/mwittrien/BetterDiscordAddons/raw/master/Plugins/CompleteTimestamps/CompleteTimestamps.plugin.js
+wget -N https://github.com/mwittrien/BetterDiscordAddons/raw/master/Plugins/LastMessageDate/LastMessageDate.plugin.js
+wget -N https://github.com/mwittrien/BetterDiscordAddons/raw/master/Plugins/ServerHider/ServerHider.plugin.js
 # wget -N https://github.com/megamit/repository/raw/gh-pages/renamer/1.5.4/renamer.plugin.js
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/AccountDetailsPlus/AccountDetailsPlus.plugin.js
+wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/BetterRoleColors/BetterRoleColors.plugin.js
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/PermissionsViewer/PermissionsViewer.plugin.js
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/RoleMembers/RoleMembers.plugin.js
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/DoNotTrack/DoNotTrack.plugin.js
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/BetterFormattingRedux/BetterFormattingRedux.plugin.js
 wget -N https://github.com/rauenzi/BDPluginLibrary/raw/master/release/0PluginLibrary.plugin.js
-
+# wget -N https://gist.github.com/samogot/355ba0ff414a6a9551baff500b1af60e/raw/4251658b52b9591ea6bbd38e1a5309ffd101e266/count-unread.plugin.js
 wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/1lib_discord_internals.plugin.js
 wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/no_send_scroll.plugin.js
 wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/quoter.plugin.js
 
 cd "$DIR/themes"
-wget -N https://github.com/Snow-Pyon/BDStuff/raw/master/themes/BMDAddon/BMDAddon.theme.css
 # wget -N https://github.com/BeardDesign1/Material-design-theme/raw/themes/Beard\'s-Material-theme-for-Discord.theme.css
-wget -N https://raw.githubusercontent.com/rauenzi/Nox/master/release/Nox.theme.css
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/themes/showURLs.theme.css
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/themes/compactUserList.theme.css
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Themes/RadialStatus/RadialStatus.theme.css
+wget -N https://github.com/rauenzi/Nox/raw/master/release/Nox.theme.css
+wget -N https://github.com/Snow-Pyon/BDStuff/raw/master/themes/BMDAddon/BMDAddon.theme.css

796d6fb90951b11579e8e4fe1a65b7d0751b1d02
FichteFoll <fichtefoll2@googlemail.com>
2019-08-29 02:38:38 +0200

    [st] Rename cycle_panes to cycle_groups

--- sublime-text/.config/sublime-text-3/Packages/User/cycle_groups.py
+++ sublime-text/.config/sublime-text-3/Packages/User/cycle_groups.py
@@ -1,7 +1,7 @@
 import sublime_plugin
 
 
-class CyclePanesCommand(sublime_plugin.WindowCommand):
+class CycleGroupsCommand(sublime_plugin.WindowCommand):
     def run(self):
         w = self.window
         w.focus_group((w.active_group() + 1) % w.num_groups())

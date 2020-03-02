803daac65b389377680d88e5d3766e350474ffec
FichteFoll <fichtefoll2@googlemail.com>
2020-03-02 15:49:01 +0100

    [mpv] Add yadif binding and sponsorblock script

--- mpv/.config/mpv/_update.sh
+++ mpv/.config/mpv/_update.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# TODO show diffs
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$DIR/shaders"
-wget -N https://github.com/bjin/mpv-prescalers/raw/master/nnedi3-nns16-win8x4.hook
-wget -N https://github.com/bjin/mpv-prescalers/raw/master/nnedi3-nns32-win8x4.hook
+# nnedi from mpv-prescalers-git package
+#wget -N https://github.com/bjin/mpv-prescalers/raw/master/nnedi3-nns16-win8x4.hook
+#wget -N https://github.com/bjin/mpv-prescalers/raw/master/nnedi3-nns32-win8x4.hook
 wget -N https://gist.github.com/igv/a015fc885d5c22e6891820ad89555637/raw/d993a9294725f04812f2759a47438d95eb26f3b5/KrigBilateral.glsl
 wget -N https://pastebin.com/raw/yacMe6EZ -O noise_static_luma.hook
 
@@ -20,3 +19,5 @@ wget -N https://raw.githubusercontent.com/SteveJobzniak/mpv-tools/master/scripts
 wget -N https://raw.githubusercontent.com/wiiaboo/mpv-scripts/master/auto-profiles.lua
 wget -N https://raw.githubusercontent.com/4e6/mpv-reload/master/reload.lua
 wget -N https://raw.githubusercontent.com/jgreco/mpv-youtube-quality/master/youtube-quality.lua
+wget -N https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock.lua
+wget https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/shared/sponsorblock.py -O shared/sponsorblock.py

07e137b2721a6d77d9fe7becc71c7f47d1b52993
FichteFoll <fichtefoll2@googlemail.com>
2020-03-30 12:57:33 +0200

    [mpv] Fix setup script

--- mpv/.config/mpv/_update.sh
+++ mpv/.config/mpv/_update.sh
@@ -2,6 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir -p "$DIR/scripts"
 cd "$DIR/shaders"
 # nnedi from mpv-prescalers-git package
 #wget -N https://github.com/bjin/mpv-prescalers/raw/master/nnedi3-nns16-win8x4.hook
@@ -9,6 +10,7 @@ cd "$DIR/shaders"
 wget -N https://gist.github.com/igv/a015fc885d5c22e6891820ad89555637/raw/d993a9294725f04812f2759a47438d95eb26f3b5/KrigBilateral.glsl
 wget -N https://pastebin.com/raw/yacMe6EZ -O noise_static_luma.hook
 
+mkdir -p "$DIR/scripts"
 cd "$DIR/scripts"
 wget -N https://raw.githubusercontent.com/ElegantMonkey/mpv-webm/master/build/webm.lua
 wget -N https://raw.githubusercontent.com/jonniek/mpv-playlistmanager/master/playlistmanager.lua
@@ -19,5 +21,8 @@ wget -N https://raw.githubusercontent.com/SteveJobzniak/mpv-tools/master/scripts
 wget -N https://raw.githubusercontent.com/wiiaboo/mpv-scripts/master/auto-profiles.lua
 wget -N https://raw.githubusercontent.com/4e6/mpv-reload/master/reload.lua
 wget -N https://raw.githubusercontent.com/jgreco/mpv-youtube-quality/master/youtube-quality.lua
-wget -N https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock.lua
-wget https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/shared/sponsorblock.py -O shared/sponsorblock.py
+
+mkdir -p "sponsorblock/shared"
+cd sponsorblock
+wget -N https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock/main.lua
+wget https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock/shared/sponsorblock.py -O shared/sponsorblock.py

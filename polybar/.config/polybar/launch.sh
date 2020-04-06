ad5f269025c8c5a0dca43ede92a29d2e75fd89a0
FichteFoll <fichtefoll2@googlemail.com>
2020-04-06 15:41:46 +0200

    [polybar] Determine outputs via xrandr

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,8 +6,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
-# Launch bars
-MONITOR=HDMI-A-0 polybar primary &
-MONITOR_2=DVI-I-1 polybar secondary &
+# Launch bars, based on xrandr output
+xrandr | grep '\bconnected\b' | \
+    while read -r output state rest; do
+        if [[ $rest == *primary* ]]; then
+            bar=primary
+        else
+            bar=secondary
+        fi
+        MONITOR=$output polybar $bar &
+    done
 
 echo "Bars launched..."

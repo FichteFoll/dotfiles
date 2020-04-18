51e0e77d78bcf932aff05833fab8a5db96ac5dd9
FichteFoll <fichtefoll2@googlemail.com>
2020-04-18 14:27:44 +0200

    [mpv] Update default-input.conf

--- mpv/.config/mpv/_get_default-input.sh
+++ mpv/.config/mpv/_get_default-input.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-# curl -L https://github.com/mpv-player/mpv/raw/master/etc/input.conf -o default-input_.conf
 perl -pe 's/^#(?!#|\s)//g' < /usr/share/doc/mpv/input.conf > default-input.conf
-# rm default-input_.conf

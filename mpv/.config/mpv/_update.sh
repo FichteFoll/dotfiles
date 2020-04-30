5ba2dcf9582ffbe2b993561e36d07dff26936533
FichteFoll <fichtefoll2@googlemail.com>
2020-04-30 12:15:08 +0200

    [mpv] Update download file

--- mpv/.config/mpv/_update.sh
+++ mpv/.config/mpv/_update.sh
@@ -4,10 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p "$DIR/scripts"
 cd "$DIR/shaders"
-# nnedi from mpv-prescalers-git package
-#wget -N https://github.com/bjin/mpv-prescalers/raw/master/nnedi3-nns16-win8x4.hook
-#wget -N https://github.com/bjin/mpv-prescalers/raw/master/nnedi3-nns32-win8x4.hook
-wget -N https://gist.github.com/igv/a015fc885d5c22e6891820ad89555637/raw/d993a9294725f04812f2759a47438d95eb26f3b5/KrigBilateral.glsl
+wget -N https://gist.githubusercontent.com/igv/a015fc885d5c22e6891820ad89555637/raw/KrigBilateral.glsl
 wget -N https://pastebin.com/raw/yacMe6EZ -O noise_static_luma.hook
 
 mkdir -p "$DIR/scripts"
@@ -21,6 +18,7 @@ wget -N https://raw.githubusercontent.com/SteveJobzniak/mpv-tools/master/scripts
 wget -N https://raw.githubusercontent.com/wiiaboo/mpv-scripts/master/auto-profiles.lua
 wget -N https://raw.githubusercontent.com/4e6/mpv-reload/master/reload.lua
 wget -N https://raw.githubusercontent.com/jgreco/mpv-youtube-quality/master/youtube-quality.lua
+wget -N https://raw.githubusercontent.com/darsain/uosc/master/uosc.lua
 
 mkdir -p "sponsorblock/shared"
 cd sponsorblock

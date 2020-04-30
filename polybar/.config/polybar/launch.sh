9fb6c5b26c621ae74e61e69385b010f0850e814f
FichteFoll <fichtefoll2@googlemail.com>
2020-04-30 12:18:37 +0200

    [polybar] Find network device names in launch script

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
+export POLY_WLAN=$(ip a | rg '^\d*:\s(w.*?):.*BROADCAST.*' -r '$1')
+export POLY_ETH=$(ip a | rg '^\d*:\s(e.*?):.*BROADCAST.*' -r '$1')
+
 # Launch bars, based on xrandr output
 xrandr | grep '\bconnected\b' | \
     while read -r output state rest; do
@@ -14,7 +17,7 @@ xrandr | grep '\bconnected\b' | \
         else
             bar=secondary
         fi
-        MONITOR=$output polybar $bar &
+        POLY_MONITOR=$output polybar $bar &
     done
 
 echo "Bars launched..."

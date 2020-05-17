a8fbdbea62efc6e1d0d75d2b2295d32141c639c7
FichteFoll <fichtefoll2@googlemail.com>
2020-05-17 23:51:24 +0200

    [mpv] Update _update.sh

--- mpv/.config/mpv/_update.sh
+++ mpv/.config/mpv/_update.sh
@@ -12,7 +12,7 @@ cd "$DIR/scripts"
 wget -N https://raw.githubusercontent.com/ElegantMonkey/mpv-webm/master/build/webm.lua
 wget -N https://raw.githubusercontent.com/jonniek/mpv-playlistmanager/master/playlistmanager.lua
 wget -N https://raw.githubusercontent.com/occivink/mpv-scripts/master/scripts/seek-to.lua
-wget -N https://raw.githubusercontent.com/SteveJobzniak/mpv-tools/master/scripts/cycle-video-rotate.lua
+wget -N https://raw.githubusercontent.com/VideoPlayerCode/mpv-tools/master/scripts/cycle-video-rotate.lua
 # has local changes:
 # wget -N https://raw.githubusercontent.com/torque/mpv-progressbar/build/progressbar.lua
 wget -N https://raw.githubusercontent.com/wiiaboo/mpv-scripts/master/auto-profiles.lua
@@ -20,7 +20,8 @@ wget -N https://raw.githubusercontent.com/4e6/mpv-reload/master/reload.lua
 wget -N https://raw.githubusercontent.com/jgreco/mpv-youtube-quality/master/youtube-quality.lua
 wget -N https://raw.githubusercontent.com/darsain/uosc/master/uosc.lua
 
-mkdir -p "sponsorblock/shared"
-cd sponsorblock
-wget -N https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock/main.lua
-wget https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock/shared/sponsorblock.py -O shared/sponsorblock.py
+wget -N https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock.lua
+mkdir -p sponsorblock_shared
+cd sponsorblock_shared
+wget -N https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock_shared/main.lua
+wget -N https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock_shared/sponsorblock.py

bb07c24c66c18130d5005fd6c8c3c0a941c30c82
FichteFoll <fichtefoll2@googlemail.com>
2021-04-07 01:56:43 +0200

    [BD] Update script list

--- BetterDiscord/.config/BetterDiscord/_update.sh
+++ BetterDiscord/.config/BetterDiscord/_update.sh
@@ -5,11 +5,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR/plugins"
 # wget -N https://github.com/Arashiryuu/crap/raw/master/greenText.plugin.js
 wget -N https://github.com/Finicalmist/CopyCode/raw/master/copyCode.plugin.js
-# wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/enableReactDevtools.plugin.js
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/plugins/linkProfilePicture.plugin.js
 # wget -N https://github.com/Jiiks/BetterDiscordApp/raw/master/Plugins/dblClickEdit.plugin.js
-wget -N https://github.com/kosshishub/BetterCustomCSS-BD-plugin/raw/master/BetterCustomCSS.plugin.js
+wget -N https://github.com/jaimeadf/BetterDiscordPlugins/raw/main/WhoReacted/WhoReacted.plugin.js
+wget -N https://github.com/l0c4lh057/BetterDiscordStuff/raw/master/Plugins/ChannelTabs/ChannelTabs.plugin.js
+wget -N https://github.com/l0c4lh057/BetterDiscordStuff/raw/master/Plugins/TypingIndicator/TypingIndicator.plugin.js
 wget -N https://github.com/Metalloriff/BetterDiscordPlugins/raw/master/DoubleClickVoiceChannels.plugin.js
+wget -N https://github.com/mwittrien/BetterDiscordAddons/raw/master/Plugins/DisplayServersAsChannels/DisplayServersAsChannels.plugin.js
+wget -N https://github.com/mwittrien/BetterDiscordAddons/raw/master/Plugins/EditUsers/EditUsers.plugin.js
+wget -N https://github.com/mwittrien/BetterDiscordAddons/raw/master/Plugins/MessageUtilities/MessageUtilities.plugin.js
+wget -N https://github.com/mwittrien/BetterDiscordAddons/raw/master/Plugins/RemoveBlockedMessages/RemoveBlockedMessages.plugin.js
 wget -N https://github.com/mwittrien/BetterDiscordAddons/raw/master/Plugins/ServerHider/ServerHider.plugin.js
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/AccountDetailsPlus/AccountDetailsPlus.plugin.js
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/BetterRoleColors/BetterRoleColors.plugin.js
@@ -17,16 +22,13 @@ wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/HideDi
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/PermissionsViewer/PermissionsViewer.plugin.js
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/RoleMembers/RoleMembers.plugin.js
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/DoNotTrack/DoNotTrack.plugin.js
-# wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Plugins/BetterFormattingRedux/BetterFormattingRedux.plugin.js
 wget -N https://github.com/rauenzi/BDPluginLibrary/raw/master/release/0PluginLibrary.plugin.js
-# wget -N https://gist.github.com/samogot/355ba0ff414a6a9551baff500b1af60e/raw/4251658b52b9591ea6bbd38e1a5309ffd101e266/count-unread.plugin.js
 # wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/1lib_discord_internals.plugin.js
-# wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/no_send_scroll.plugin.js
-# wget -N https://github.com/samogot/betterdiscord-plugins/raw/master/v1/quoter.plugin.js
 wget -N https://github.com/Strencher/BetterDiscordStuff/raw/master/SuppressReplyMentions/SuppressReplyMentions.plugin.js
+wget -N https://github.com/Strencher/BetterDiscordStuff/raw/master/PlatformIndicators/APlatformIndicators.plugin.js
+
 
 cd "$DIR/themes"
 wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/themes/showURLs.theme.css
-# wget -N https://github.com/Inve1951/BetterDiscordStuff/raw/master/themes/compactUserList.theme.css
 wget -N https://github.com/rauenzi/BetterDiscordAddons/raw/master/Themes/RadialStatus/RadialStatus.theme.css
 wget -N https://github.com/rauenzi/Nox/raw/master/release/Nox.theme.css

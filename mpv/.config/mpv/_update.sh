1d61f440dfc618a01f1ac23093a05c98613a0234
FichteFoll <fichtefoll2@googlemail.com>
2022-09-21 20:28:35 +0200

    [mpv] Order script downloads lexicographically

--- mpv/.config/mpv/_update.sh
+++ mpv/.config/mpv/_update.sh
@@ -11,15 +11,13 @@ mkdir -p "$DIR/scripts"
 cd "$DIR/scripts"
 # wget -N https://gist.githubusercontent.com/CyberShadow/2f71a97fb85ed42146f6d9f522bc34ef/raw/autosave.lua
 wget -N https://github.com/ekisu/mpv-webm/releases/download/latest/webm.lua
+wget -N https://raw.githubusercontent.com/4e6/mpv-reload/master/reload.lua
+wget -N https://raw.githubusercontent.com/jgreco/mpv-youtube-quality/master/youtube-quality.lua
 wget -N https://raw.githubusercontent.com/jonniek/mpv-playlistmanager/master/playlistmanager.lua
 wget -N https://raw.githubusercontent.com/jonniek/mpv-scripts/master/appendURL.lua
 wget -N https://raw.githubusercontent.com/occivink/mpv-scripts/master/scripts/seek-to.lua
-wget -N https://raw.githubusercontent.com/VideoPlayerCode/mpv-tools/master/scripts/cycle-video-rotate.lua
-# replaced with uosc
-# wget -N https://raw.githubusercontent.com/torque/mpv-progressbar/build/progressbar.lua
-wget -N https://raw.githubusercontent.com/4e6/mpv-reload/master/reload.lua
-wget -N https://raw.githubusercontent.com/jgreco/mpv-youtube-quality/master/youtube-quality.lua
 wget -N https://raw.githubusercontent.com/tomasklaen/uosc/master/scripts/uosc.lua
+wget -N https://raw.githubusercontent.com/VideoPlayerCode/mpv-tools/master/scripts/cycle-video-rotate.lua
 
 wget -N https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock.lua
 mkdir -p sponsorblock_shared

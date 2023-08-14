65fb0cf5415bd1781ba740d0f8fad1c2c6148668
FichteFoll <fichtefoll2@googlemail.com>
2023-08-14 12:17:24 +0200

    [polybar] Use new `interface-type` feature
    Can automatically infer an interface of the appropriate type, which is
    much more robust than my wrapper script that completely failed for
    multiple matching interfaces (because it didn't only extract the first
    result but also wouldn't know which one would be the correct one).
    
    Also don't spawn polybar processes from a sub-shell anymore.

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,18 +6,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
-export POLY_WLAN=$(ip a | rg '^\d*:\s(w.*?):.*BROADCAST.*' -r '$1')
-export POLY_ETH=$(ip a | rg '^\d*:\s(e.*?):.*BROADCAST.*' -r '$1')
-
 # Launch bars, based on xrandr output
-xrandr | grep '\bconnected\b' | \
-    while read -r output state rest; do
-        if [[ $rest == *primary* ]]; then
-            bar=primary
-        else
-            bar=secondary
-        fi
-        POLY_MONITOR=$output polybar $bar &
-    done
+inputs=$(xrandr | grep '\bconnected\b')
+while read -r output state rest; do
+    if [[ $rest == *primary* ]]; then
+        bar=primary
+    else
+        bar=secondary
+    fi
+    POLY_MONITOR=$output polybar $bar &
+done <<< $inputs
 
 echo "Bars launched..."
